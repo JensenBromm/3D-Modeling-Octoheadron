349b9feefb808f209cd37c358f9db7b5cb464c4f
JensenBromm <jensenbromm580@gmail.com>
2023-03-27 12:04:43 -0400

    Initial commit

---
+++ src/assignment7/Octahedron.java
@@ -0,0 +1,26 @@
+package assignment7;
+
+import javax.media.j3d.GeometryArray;
+import javax.media.j3d.IndexedTriangleArray;
+import javax.vecmath.Point3f;
+
+/**
+ *
+ * @author 
+ */
+public class Octahedron extends IndexedTriangleArray {
+
+    public Octahedron() {
+        super(6, GeometryArray.COORDINATES, 24); // 6 vertices, 3*8 indices
+
+
+
+
+
+
+
+
+
+
+    }
+}
